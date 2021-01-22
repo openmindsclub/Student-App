b1779459eae43dd8e3c1d70ccf5fee4d6e95cb79
aymenkhs <aymenkhouas@gmail.com>
2021-01-22 17:31:06 +0100

    fixing the cards

--- lib/menu_elements/schedule_scroller.dart
+++ lib/menu_elements/schedule_scroller.dart
@@ -40,44 +40,58 @@ class _ScheduleScrollerState extends State<ScheduleScroller> {
     return Card(
         margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
         color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right:12.0),
+              height: 80,
+              width: 5,
+              color: Color(0xff43B485),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Icon(
-                    Icons.title,
-                    color: Colors.black,
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.calendar_today_rounded,
+                        size: 15,
+                        color: Colors.black,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left:5.0),
+                        child: Text(
+                          task.title,
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    task.date_string(),
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Color(0xff7D7A78),
+                    ),
                   ),
                   Text(
-                    task.title,
+                    task.description,
                     style: TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0,
+                      color: Color(0xff7D7A78),
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 6.0),
-              Text(
-                task.date_string(),
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Color(0xff7D7A78),
-                ),
-              ),
-              Text(
-                task.description,
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Color(0xff7D7A78),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         )
     );
   }
