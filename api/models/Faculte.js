21e0da3d8862fa03720932a931189d04c099142a
Aicha Aya <mebarkiaichaaya03@gmail.com>
2021-01-25 13:13:54 +0100

    added Faculte model

---
+++ api/models/Faculte.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const fcltSchema = new mongoose.Schema({
+    nom : {
+        type : String,
+        required : true
+    }
+} , {timestamps : true })
+
+const Faculte = mongoose.model('Faculte' , fcltSchema);
+
+module.exports = Faculte;
