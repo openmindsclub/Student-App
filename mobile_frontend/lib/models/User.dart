af143b1c2f60c5e48bb0d974d5ddd82a700d5f38
aymmenkhs <aymenkhouas@gmail.com>
2021-02-09 16:12:20 +0100

    User init

--- mobile_frontend/lib/models/User.dart
+++ mobile_frontend/lib/models/User.dart
@@ -1,3 +1,16 @@
-class User {
+import 'enumeration.dart';
 
+class User {
+  String email, lastName, firstName;
+  String id; // matricule
+  String password;
+
+  Faculty faculty;
+  StudyYear studyYear;
+  String speciality, section, group;
+
+  User.registerConstructor(this.email, this.password, this.lastName, this.firstName, this.id,
+      this.faculty, this.studyYear, this.speciality, this.section, this.group);
+
+  User.loginConstructor(this.email, this.password);
 }
