5fe9b8706c1734617157ac2328acf11ebcdd4585
Aicha Aya <mebarkiaichaaya03@gmail.com>
2021-02-21 11:54:37 +0100

    modified constraints

--- api/models/Faculte.js
+++ api/models/Faculte.js
@@ -1,10 +1,15 @@
+const Specialite = require('./Specialite');
 const mongoose = require('mongoose');
 
 const fcltSchema = new mongoose.Schema({
     nom : {
         type : String,
         required : true
-    }
+    },
+    specialites : [{
+        type : Specialite.schema,
+        required : true
+    }]
 } , {timestamps : true })
 
 const Faculte = mongoose.model('Faculte' , fcltSchema);
