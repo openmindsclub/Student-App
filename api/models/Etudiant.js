659f1e2126ed2b7302040bd7cd8a430fa694584d
Aicha Aya <mebarkiaichaaya03@gmail.com>
2021-02-21 11:54:09 +0100

    modified constraints

--- api/models/Etudiant.js
+++ api/models/Etudiant.js
@@ -1,26 +1,17 @@
 const mongoose = require('mongoose');
 const Personne = require ('./Personne');
-const Groupe = require ('./Groupe');
-const Task = require('./Task');
 
 const etudiantSchema = new mongoose.Schema({
 
     matricule : {
         type : String,
-        required : true
+        required : true,
+        unique : true
     },
     personne : {
         type : Personne.schema,
         required : true
-    },
-    groupe : {
-        type : Groupe.schema,
-        required : true
-    },
-    tasks : [{
-        type : Task.schema,
-        required : true
-    }],
+    }
 } , {timestamps : true })
 
 const Etudiant = mongoose.model('Etudiant' , etudiantSchema);
