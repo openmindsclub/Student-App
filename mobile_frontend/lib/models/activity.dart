e86c45133a15d9ce43c519d134af7c67b8bfd903
aymenkhs <aymenkhouas@gmail.com>
2021-07-27 19:28:09 +0100

    making activity and category distenction

--- mobile_frontend/lib/models/activity.dart
+++ mobile_frontend/lib/models/activity.dart
@@ -2,6 +2,7 @@ class Activity{
   String type;
   DateTime date;
   String module;
+  String time;
 
-  Activity({this.type, this.date, this.module});
+  Activity({this.type, this.date, this.time, this.module});
 }
